9bd8efa2a480ef9fc1739a05e1a9721cf44a66ca
Jona <zS19023589@estudiantes.uv.mx>
2023-06-14 18:44:05 -0600

    a

--- Pages/LoginEmpleado.cshtml.cs
+++ Pages/LoginEmpleado.cshtml.cs
@@ -7,44 +7,44 @@ namespace UvEats_Aplicaciones.Pages;
 
 public class LoginModel : PageModel
 {
-    private readonly ILogger<LoginModel> _logger;
-    //public   ILoginProvider _login {get; set;}
+    // private readonly ILogger<LoginModel> _logger;
+    // //public   ILoginProvider _login {get; set;}
 
-    [BindProperty]
-    public String login {get; set;}
+    // [BindProperty]
+    // public String login {get; set;}
 
-    [BindProperty]
-    public String password {get; set;}
+    // [BindProperty]
+    // public String password {get; set;}
 
-    public LoginModel(/*ILogger<LoginModel> logger,                        
-                        ILoginProvider login*/)
-    {
-        // _logger = logger;
-        // _login = login;        
-    }
+    // public LoginModel(/*ILogger<LoginModel> logger,                        
+    //                     ILoginProvider login*/)
+    // {
+    //     // _logger = logger;
+    //     // _login = login;        
+    // }
 
-    public void OnGet()
-    {
+    // public void OnGet()
+    // {
 
-    }    
+    // }    
     
 
-    public void OnPostLogin()
-    {        
-        // if (_login != null && !String.IsNullOrEmpty(login) && !String.IsNullOrEmpty(password)) {
-        //     var datosSesion = _login.iniciarSesion(login, password);
-        //     _logger.LogInformation($"Login: {login}");
-        //     _logger.LogInformation($"Password: {password}");
-        //     _logger.LogInformation($"Respuesta: {datosSesion}");
-        //     if (datosSesion != null) {
-        //         Request.HttpContext.Session.SetString("login", datosSesion.login);
-        //         Request.HttpContext.Session.SetString("nombre", datosSesion.nombreCompleto);
-        //         Request.HttpContext.Session.SetString("token", datosSesion.token);
-        //        // Response.Redirect("CursosCard");
-        //     }else
-            Console.WriteLine("pasoooo");
+    // public void OnPostLogin()
+    // {        
+    //     // if (_login != null && !String.IsNullOrEmpty(login) && !String.IsNullOrEmpty(password)) {
+    //     //     var datosSesion = _login.iniciarSesion(login, password);
+    //     //     _logger.LogInformation($"Login: {login}");
+    //     //     _logger.LogInformation($"Password: {password}");
+    //     //     _logger.LogInformation($"Respuesta: {datosSesion}");
+    //     //     if (datosSesion != null) {
+    //     //         Request.HttpContext.Session.SetString("login", datosSesion.login);
+    //     //         Request.HttpContext.Session.SetString("nombre", datosSesion.nombreCompleto);
+    //     //         Request.HttpContext.Session.SetString("token", datosSesion.token);
+    //     //        // Response.Redirect("CursosCard");
+    //     //     }else
+    //         Console.WriteLine("pasoooo");
         
-    }
+    // }
 
     
 }
